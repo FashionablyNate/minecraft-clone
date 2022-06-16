2363708d113382fa22ae2ab5b0b7429179310292
FashionablyNate <nate@nathanspelts.com>
2022-06-16 14:49:43 -0700

    window creation

--- src/main.cpp
+++ src/main.cpp
@@ -2,66 +2,118 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-#define WIDTH 800
-#define HEIGHT 600
+unsigned int window_width = 800;
+unsigned int window_height = 600;
 
-void processInput(GLFWwindow *window);
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+// prints description of last thrown error
+void display_glfw_error_message();
+
+// called upon window resize, changes the viewport size
+void framebuffer_size_callback(GLFWwindow * window, int width, int height);
+
 
 int main() {
 
-    // we set the opengl version in glfw and tell it we're using the core profile
-    glfwInit();
+    /* Initializing GLFW */
+    /* ---------------------------------------------------------------------- */
+
+    // attempt to initialize glfw
+    if (glfwInit() == GLFW_FALSE) {
+        display_glfw_error_message();
+        return -1;
+    }
+
+    // specify the version number for opengl, opengl version 3.3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    // specify the opengl profile, opengl core
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // we create a window object and store a pointer to it
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Minecraft-Clone", NULL, NULL);
+
+    /* Creating the window */
+    /* ---------------------------------------------------------------------- */
+
+    // here we create a window object which stores all windowing data
+    GLFWwindow * window = glfwCreateWindow(window_width,
+                                           window_height,
+                                           "Minecraft-Clone", // window title
+                                           NULL, // NULL specifies windowed mode
+                                           NULL);
     
-    // if window is null, give up
+    // if the window wasn't created, we print an error and quit
     if (window == NULL) {
-        std::cout << "Failed to create GLFW window." << std::endl;
+        display_glfw_error_message();
         glfwTerminate();
         return -1;
     }
 
-    // tell glfw to use this window object we just created
+    // Makes the OpenGL context of the specified window current on the calling thread
     glfwMakeContextCurrent(window);
+    
 
-    // we pass GLAD the function to load the address of OpenGL function pointers
-    // this is OS specific, glfw gives us glfwGetProcAddress which defines the
-    // correct function based on the OS we compile for
+    /* Initializing GLAD */
+    /* ---------------------------------------------------------------------- */
+
+    // query glad for the opengl function pointers, if it fails we print an error
+    // and quit.
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cout << "Failed to initialize GLAD" << std::endl;
+        std::cout << "Failed to intialize GLAD" << std::endl;
+        glfwTerminate();
         return -1;
     }
 
-    glViewport(0, 0, WIDTH, HEIGHT);
+    /* Creating the viewport */
+    /* ---------------------------------------------------------------------- */
 
+    glViewport(0,               // set the x coord of the lower left corner
+               0,               // set the y coord of the lower left corner
+               window_width,    // set the x coord of the upper right corner
+               window_height);  // set the y coord of the upper right corner
+
+    // register a callback function for when the window is resized
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    /* Render Loop */
+    /* ---------------------------------------------------------------------- */
+    
+    // checks if window should close at every iteration of the loop
     while(!glfwWindowShouldClose(window)) {
 
-        processInput(window);
-
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwPollEvents();
+        // swaps the color buffer from the back buffer to the front buffer
         glfwSwapBuffers(window);
+
+        // checks if events have occurred (like window state update, or inputs)
+        // and calls the appropriate callback function.
+        glfwPollEvents();
     }
 
     glfwTerminate();
-
     return 0;
 }
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    glViewport(0, 0, width, height);
+/* -------------------------------------------------------------------------- */
+// function for printing out the last glfw error message
+void display_glfw_error_message() {
+
+    // create memory space for the description
+    const char * description;
+
+    // retrieve the last error thrown, and store it in the description memory address
+    int code = glfwGetError(&description);
+
+    // if anything got stored in description, we print it out
+    if (description) {
+        std::cout << "Error: " << description << std::endl;
+    }
 }
 
-void processInput(GLFWwindow *window) {
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
+/* -------------------------------------------------------------------------- */
+// called upon window resize, changes the viewport size
+void framebuffer_size_callback(GLFWwindow * window, int width, int height) {
+    
+    glViewport(0,       // set the x coord of the lower left corner
+               0,       // set the y coord of the lower left corner
+               width,   // set the x coord of the upper right corner
+               height); // set the y coord of the upper right corner
 }
